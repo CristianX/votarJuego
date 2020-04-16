cdc5b90aab7b6b96bc9aa8d5c89492e5b16450ec
Cristian Xavier Tapia Vargas <thecristianx@hotmail.com>
2020-04-15 19:01:05 -0500

    Quitando información vulnerable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,15 @@
 
 export const environment = {
   production: false,
-  url : 'http://localhost:5000/firestore-grafica-6feb1/us-central1',
+  url : '',
   firebase: {
-    apiKey: 'AIzaSyAyBuEclgfZdXw3-qZfa8kWxftSVRdA1pQ',
-    authDomain: 'firestore-grafica-6feb1.firebaseapp.com',
-    databaseURL: 'https://firestore-grafica-6feb1.firebaseio.com',
-    projectId: 'firestore-grafica-6feb1',
-    storageBucket: 'firestore-grafica-6feb1.appspot.com',
-    messagingSenderId: '936940680712',
-    appId: '1:936940680712:web:7f2e379ef56d47fa3443bd'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: ''
   }
 };
 
